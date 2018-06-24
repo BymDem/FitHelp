234408d448ace8aaf779ddfeb7d9b447321d389f
BymDem <Caznach@gmail.com>
2018-06-24 23:43:25 +1100

    Icons, dbhelper

--- app/src/main/java/com/example/fithelp/MainActivity.java
+++ app/src/main/java/com/example/fithelp/MainActivity.java
@@ -17,14 +17,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_train:
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
