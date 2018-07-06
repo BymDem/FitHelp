21ce62029ac67b47cbc37080c6a86a410345215b
Bymka <Cazzznach@gmail.com>
2018-07-06 20:38:28 +1000

    хзхз

--- app/src/main/java/com/example/fithelp/MainActivity.java
+++ app/src/main/java/com/example/fithelp/MainActivity.java
@@ -77,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_button, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
